835a2ef1b10121df36006bde0f09b44bbb7cd788
ZeroHX <61070023@kmitl.ac.th>
2019-04-15 20:38:15 +0700

    Replace pass with star

--- project/pro.c
+++ project/pro.c
@@ -21,13 +21,14 @@ ________________________________________________________________________________
 #include <conio.h>
 /*
 Structure of all Record profile
-[ NOTE: THERE IS A SAMPLE TEMPLATE FOR TESTING!! ]
+[ NOTE: THIS IS A SAMPLE TEMPLATE FOR TESTING!! ]
 */
 struct Record{
-    char name[20];
-    char last[20];
-    char user[20];
-    char pass[20];
+    char name[20];      // Name
+    char last[20];      // Lastname
+    char user[20];      // Username
+    char pass[20];      // Password
+    char career[20];    // [ Student or Teacher ]
 }rec1;
 
 //Function Declare here!!
@@ -53,10 +54,31 @@ void login(){
     int status = 0;     //check User & Pass state (0/1)
     system("cls");      //For Clear screen
     printf("Login To STD-Program!!\n\n");
+
+    /*USERNAME Input*/
     printf("Username: ");
     scanf("%s", log_id);
+
+    /*PASSWORD Input*/
     printf("\nPassword: ");
-    scanf("%s", log_pass);
+    int i = 0;
+    while(1){
+        char ch = getch();  //Get input from Keyboard (KEYCODE)
+        if(ch == 13 || ch == 9){    //13 is ENTER & 9 is TAB
+            log_pass[i] = '\0';    //Set the end point of String
+            break;
+        }
+        else if(ch == 8){   //8 is BackSpace (for del)
+            if(i>0){
+                i--;
+                printf("\b \b");    //NOTE: Keep space with \b
+            }
+        }
+        else{       //Replace password with star(*)
+            log_pass[i++] = ch;
+            printf("* \b");
+        }
+    }
 
     /* Method to read .txt file (googling plz)*/
     FILE *fp;
@@ -69,8 +91,10 @@ void login(){
         }
     }
     if(status == 1){
+        system("cls");
         printf("Login Successful!!\n\n");
         printf("Welcome back %s !!\n\n", rec1.name);
+        printf("You are %s !!\n\n", rec1.career);
         fclose(fp);
     }
     else{
@@ -93,20 +117,55 @@ void regis(){
     else{
         system("cls");
         printf("Please Enter Your Information Correctly\n");
-        /* NAME-LASTNAME*/
+        /* NAME-LASTNAME */
         printf("Name: ");
         scanf("%s",&rec1.name);
         printf("LastName: ");
         scanf("%s",&rec1.last);
 
-        /*USERNAME-PASSWORD*/
+        /* CAREER [teacher or student] */
+        int choice;
+        printf("Career      1: Teacher\n");
+        printf("            2: Student\n\n");
+        printf("Choose your choice![num]>>>");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                strcpy(rec1.career, "Teacher");
+                break;
+            case 2:
+                strcpy(rec1.career, "Student");
+                break;
+        }
+
+        /* USERNAME */
         printf("Username: ");
         scanf("%s",&rec1.user);
+
+        /* PASSWORD */
         printf("Password: ");
-        scanf("%s",&rec1.pass);
+        int i = 0;
+        while(1){
+            char ch = getch();  //Get input from Keyboard (KEYCODE)
+            if(ch == 13 || ch == 9){    //13 is ENTER & 9 is TAB
+                rec1.pass[i] = '\0';    //Set the end point of String
+                break;
+            }
+            else if(ch == 8){   //8 is BackSpace (for del)
+                if(i>0){
+                    i--;
+                    printf("\b \b");    //NOTE: Keep space with \b
+                }
+            }
+            else{       //Replace password with star(*)
+                rec1.pass[i++] = ch;
+                printf("* \b");
+            }
+        }
 
         printf("------------------------------------------\n");
         printf("Name: %s %s\n",rec1.name,rec1.last);
+        printf("Career: %s\n", rec1.career);
         printf("Username: %s\n",rec1.user);
         printf("Password: ");
         for(int i=0; i<strlen(rec1.pass);i++){
@@ -131,7 +190,6 @@ void regis(){
                 regis();
                 break;
         }
-
     }
 }
 
